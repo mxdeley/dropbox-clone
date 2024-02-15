ff6df013f3b5a4dd6c5dbb0c68ec749f75512261
Dan <dev@madeleydesignstudio.org>
2024-02-15 20:29:22 +0000

    first push

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Logo from "./Logo.svelte";
+    import * as Avatar from "$lib/components/ui/avatar";
+	import Button from "./ui/button/button.svelte";
+
+</script>
+<header class="flex items-center justify-between bg-background sticky top-0 left-0">
+    <!-- Logo -->
+    <Logo/>
+    <!-- Avatar -->
+    <div class="flex items-center gap-5">
+        <Avatar.Root>
+            <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+            <Avatar.Fallback>CN</Avatar.Fallback>
+        </Avatar.Root>
+        <Button variant="ghost">Sign Out</Button>
+    </div>
+    <!-- Logout -->
+</header>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Button from "$lib/components/ui/button/button.svelte";
+	import { ArrowRight } from "lucide-svelte";
+
+</script>
+<div class="bg-[#1e1919]">
+    <div class="text-white text-center pt-[72px] max-w-[777px] mx-auto">
+        <h1 class="text-3xl md:text-6xl">Securely collaborate on your content anywhere.</h1>
+        <p class="text-sm md:text-lg mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere at cum delectus libero, nesciunt ducimus error possimus voluptates unde minima eveniet neque repellat ipsa, quasi vitae in sequi inventore. Eum!</p>
+        <Button class="p-6 text-black font-semibold mt-10">Get started now <ArrowRight class="w-5 h-5"/> </Button>
+    </div>
+    <video class="mt-10" autoplay playsinline loop muted>
+		<track kind="captions" />
+		<source
+			src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/dropbox/dbx1-hero-1920x1080.mp4"
+			type="video/mp4"
+		/>
+	</video>
+</div>
